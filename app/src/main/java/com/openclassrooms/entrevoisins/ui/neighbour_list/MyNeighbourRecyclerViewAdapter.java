e7541c545b876f4ed6ad86a17c83f25ac6d76fa1
Iryuku <55204365+Iryuku@users.noreply.github.com>
2020-03-05 14:08:23 +0100

    envoi

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
@@ -0,0 +1,97 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.events.DeleteNeighbourEvent;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import android.content.Intent;
+
+public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeighbourRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Neighbour> mNeighbours;
+    Context mContext;
+    public MyNeighbourRecyclerViewAdapter(List<Neighbour> items) {
+        mNeighbours = items;
+    }
+    MyNeighbourRecyclerViewAdapter(List<Neighbour> items, Context context) {
+        mNeighbours = items;
+
+        mContext = context;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_neighbour, parent, false);
+        return new ViewHolder(view);
+        Gson gson = new Gson();
+
+        holder.itemView.setOnClickListener(v -> {
+
+            Context context= v.getContext();
+            Intent intent= new Intent(context, DetailNeighbourActivity.class);
+            intent.putExtra("Neighbour", gson.toJson(mNeighbour));
+            context.startActivity(intent);
+
+        });
+
+    }
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Neighbour neighbour = mNeighbours.get(position);
+        holder.mNeighbourName.setText(neighbour.getName());
+        Glide.with(holder.mNeighbourAvatar.getContext())
+                .load(neighbour.getAvatarUrl())
+                .apply(RequestOptions.circleCropTransform())
+                .into(holder.mNeighbourAvatar);
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent= new Intent(mContext, DetailNeighbourActivity.class);
+            intent.putExtra("Name", neighbour.getName());
+            intent.putExtra("NAME_NEIGHBOUR",neighbour.getAvatarUrl());
+            intent.putExtra("AVATAR_NEIGHBOUR",neighbour.getAvatarUrl());
+            mContext.startActivity(intent);
+        });
+
+        holder.mDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().post(new DeleteNeighbourEvent(neighbour));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNeighbours.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.item_list_avatar)
+        public ImageView mNeighbourAvatar;
+        @BindView(R.id.item_list_name)
+        public TextView mNeighbourName;
+        @BindView(R.id.item_list_delete_button)
+        public ImageButton mDeleteButton;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
